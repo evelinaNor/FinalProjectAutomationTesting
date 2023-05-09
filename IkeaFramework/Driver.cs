e284d2d8690862973ac5e3fd83694f4eee704153
evelinaNor <eva19943@gmail.com>
2023-05-09 18:12:03 +0300

    Added Driver methods

--- IkeaFramework/Driver.cs
+++ IkeaFramework/Driver.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,26 @@ namespace IkeaFramework
 {
     internal class Driver
     {
+        private static IWebDriver driver;
+
+        public static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        internal static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        internal static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static void ShutdownDriver()
+        {
+            driver.Quit();
+        }
     }
 }
