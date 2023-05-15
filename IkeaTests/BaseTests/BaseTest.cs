9e2f088abd3c299a4afa95998b9dbb3bb04088ad
evelinaNor <eva19943@gmail.com>
2023-05-15 19:08:48 +0300

    Inheritance base test class

---
+++ IkeaTests/BaseTests/BaseTest.cs
@@ -0,0 +1,19 @@
+﻿using IkeaFramework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IkeaTests.BaseTests
+{
+    internal class BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.InitializeDriver();
+        }
+    }
+}
