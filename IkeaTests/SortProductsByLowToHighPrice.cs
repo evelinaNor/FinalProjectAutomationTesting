bff6d9a512d1ff34d029aa862e5970e7b593e2fd
evelinaNor <eva19943@gmail.com>
2023-05-23 20:21:53 +0300

    ProductSortingByPrice methods

---
+++ IkeaTests/SortProductsByLowToHighPrice.cs
@@ -0,0 +1,33 @@
+﻿using IkeaFramework.Pages;
+using IkeaTests.BaseTests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IkeaTests
+{
+    internal class SortProductsByLowToHighPrice : BaseTest
+    {
+        [SetUp]
+
+        public void OpenMainPageAndCookiesAcception()
+        {
+            HomePage.Open();
+            HomePage.AcceptCookies();
+        }
+
+        [Test]
+        public void SortProductsFromLowToHighPrice()
+        {
+            ProductSortingByPricePage.OpenListOfProducts();
+            ProductSortingByPricePage.ClickOnSortFilter();
+            ProductSortingByPricePage.SelectPriceFromLowToHigh();
+
+            Assert.IsTrue(ProductSortingByPricePage.ProductsAreSortedFromLowToHighPrice());
+
+        }
+    }
+}
