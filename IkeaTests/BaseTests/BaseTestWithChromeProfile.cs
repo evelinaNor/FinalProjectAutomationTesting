7cc9314634cc40b4d01021d0a11c7105228181e3
evelinaNor <eva19943@gmail.com>
2023-05-25 20:41:21 +0300

    unnecessary using deletion for BaseTest

--- IkeaTests/BaseTests/BaseTestWithChromeProfile.cs
+++ IkeaTests/BaseTests/BaseTestWithChromeProfile.cs
@@ -1,11 +1,6 @@
 ﻿using IkeaFramework;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IkeaTests.BaseTests
 {
